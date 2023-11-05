f5f57b64fe0b5848212f25d278c4a29ec74eb71b
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-11-05 17:38:52 -0500

    added index and reaction model

--- models/thought.js
+++ models/thought.js
@@ -1,15 +1,37 @@
 const { Schema, model } = require('mongoose');
+const Reaction = require('../Reaction');
 
 const thoughtSchema = new Schema(
     {
-        thoughtText:{
+        thoughtText: {
             type: String,
             required: true,
-            max_length: 280,
             min_length: 1,
+            max_length: 280,
         },
-        createdAt:{
+        createdAt: {
             type: Date,
             default: Date.now(),
-        }
-    })
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
